befe7ff3e6d7beb1cf146007f63ff8eb2169bc65
khairunnisa <juniorprogrammer@amtis.com.my>
2014-03-20 11:57:33 +0800

    quiz

---
+++ nisa/quiz.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/quiz.css">
+<script src="js/jquery-2.1.0.min.js">
+</script>
+    <title></title>
+</head>
+<body>
+<table class="table table-bordered" id="bank">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Maybank</th>
+            <th>Bank Islam</th>
+            <th>Total</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>
+                <input type="text" id="maybank" />
+            </td>
+            <td>
+                <input type="text" id="bankislam" />
+            </td>
+            <td>
+                <input type="text" id="total" disabled="true" />
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+</body>
+</html>

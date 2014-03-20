7d11650cb1deb9042002db32e054c5d5f4c7bd81
khairunnisa <juniorprogrammer@amtis.com.my>
2014-03-20 10:12:16 +0800

    445

---
+++ quiz.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/quiz.css">
+    <title></title>
+</head>
+<body>
+<table class="table table-bordered" id="bank">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Maybank</th>
+            <th>Bank Islam</th>
+            <th>Total</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>
+                <input type="text" id="maybank" />
+            </td>
+            <td>
+                <input type="text" id="bankislam" />
+            </td>
+            <td>
+                <input type="text" id="total" disabled="true" />
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+</body>
+</html>
